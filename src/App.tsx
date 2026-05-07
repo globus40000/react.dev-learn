3444434ff09a1feb69dde8ef5c00c62423f4c62a
Pavel Ashifin <globus40000@gmail.com>
2026-05-07 23:11:45 +0600

    Displaying data

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,38 @@
 import type { FC } from "react";
 
-const MyButton: FC = () => {
-  return <button type="button">I'm a button</button>;
+interface User {
+  name: string;
+  imageUrl: string;
+  imageSize: number;
+}
+
+const user: User = {
+  name: "Hedy Lamarr",
+  imageUrl: "https://react.dev/images/docs/scientists/yXOvdOSs.jpg",
+  imageSize: 90,
+};
+
+const Profile: FC = () => {
+  return (
+    <div>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={"Photo of " + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize,
+        }}
+      />
+    </div>
+  );
 };
 
 const App: FC = () => {
   return (
     <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
+      <Profile />
     </div>
   );
 };
